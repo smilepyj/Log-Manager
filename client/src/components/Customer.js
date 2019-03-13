f177e9143384ee9c3d39af1de1839d2cb531f544
smilepyj <smilepyj87@gmail.com>
2019-03-13 16:25:10 +0900

    Implement Customer Add Module & Customer List Refresh Module

--- client/src/components/Customer.js
+++ client/src/components/Customer.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 
-class Customer extends React.Component {
+class Customer extends Component {
     render() {
         return (
             <TableRow>
